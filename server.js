b3062fc6122ea03d08090c48c1d242336c8c65e5
chall298 <chall361@gmail.com>
2021-10-11 22:11:49 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+
+// Middleware for data on post request
+app.use(express.json({extended: false}))
+
+const todos = [
+    {
+        message: "Wash Car...",
+        id: 1
+    },
+    {
+        message: "Go for a run...",
+        id: 2
+    },
+    {
+        message: "Cook dinner",
+        id: 3
+    }
+]
+
+app.get("/", (req, res) => {
+    res.status(200).json(todos)
+})
+
+app.post("/", (req, res) => {
+    const newTodo = {
+        message: req.body.message,
+        id: uuidv4()
+    }
+
+    todos.push(newTodo)
+    res.status(201).json(todos)
+})
+
+const PORT = process.env.PORT || 5001
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`)
+})
